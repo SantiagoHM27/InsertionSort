3e71e9d4894c99dde5e55c4b4bd0ee1e38c654bd
SantiagoHM27 <himejimadarkrai@gmail.com>
2023-11-15 23:45:07 -0600

    insertion 2 (ya sirve)

--- src/Insertion.java
+++ src/Insertion.java
@@ -1,18 +1,18 @@
 class Insertion{
     public static void main(String []args){
-        int []arr = {5, 3, 4, 8, 7, 5, 1, 2, 3};
+        int []arr = {10, 6, 1, 8, 7, 5, 4, 2, 3};
         for(int j = 1; j < arr.length; j++){
             int actual = arr[j];
 
             int i = j-1;
             while(i >= 0 && arr[i] > actual){
-                arr[i+1] = arr[1];
+                arr[i+1] = arr[i]; 
                 i--;
             }
             arr[i+1]=actual;
         }
         for(int elem: arr){
-            System.out.println(elem + " ");
+            System.out.print(elem + " ");
         }
     }
 }
